c8edd752d5c35f8eada1d3d920b301899f3611b0
Mir Mohatsim Billah <kawsermir920@gmail.com>
2026-06-13 18:07:57 +0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,5 @@
-<script src="https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js"></script>
-<script src="https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js"></script>
+// Firebase SDK (CDN use korbo, import na)
 
-<script>
 const firebaseConfig = {
   apiKey: "AIzaSyD22xK7PbJTKkYfmy8rQLiaA7sPLHzkCAg",
   authDomain: "nodemaps-410f8.firebaseapp.com",
@@ -11,6 +9,8 @@ const firebaseConfig = {
   appId: "1:87271462319:web:7768c6454ca14ef59b0b5a"
 };
 
+// Initialize Firebase (OLD STYLE)
 firebase.initializeApp(firebaseConfig);
+
+// Auth ready
 const auth = firebase.auth();
-</script>

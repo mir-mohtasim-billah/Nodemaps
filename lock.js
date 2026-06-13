b897d67ee34e25855c46b99f3e43a62acef2cea8
Mir Mohatsim Billah <kawsermir920@gmail.com>
2026-06-13 16:57:10 +0600

    Update lock.js

--- lock.js
+++ lock.js
@@ -1,1 +1,14 @@
+function lockNode(projectId, nodeId, uid){
+  db.collection("projects").doc(projectId).update({
+    [`locks.${nodeId}`]: {
+      user: uid,
+      time: Date.now()
+    }
+  });
+}
 
+function unlockNode(projectId, nodeId){
+  db.collection("projects").doc(projectId).update({
+    [`locks.${nodeId}`]: firebase.firestore.FieldValue.delete()
+  });
+}
